31677a02aabf8dcdbf72ddd5d36c2729cc0179df
Jas1thCS <Jas1thCS>
2025-06-19 19:12:42 +0000

    Changed config file

--- Formula1/configuration.py
+++ Formula1/configuration.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
-raw_folder_path = 'abfss://raw@adfprojectdatalake.dfs.core.windows.net'
-processed_folder_path = 'abfss://processed@adfprojectdatalake.dfs.core.windows.net'
-presentation_folder_path = 'abfss://presentation@adfprojectdatalake.dfs.core.windows.net/'
+#Configure your folder paths over here!
+raw_folder_path = 'abfss://<container>@<storage_account>.dfs.core.windows.net'
+processed_folder_path = 'abfss://<container>@<storage_account>.dfs.core.windows.net'
+presentation_folder_path = 'abfss://<container>@<storage_account>.dfs.core.windows.net/'
